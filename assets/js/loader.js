446ba771cbd68b8fe2e10a6a09a59f05d7ba87ab
Filip Mertens <bunsenstraat@gmail.com>
2020-01-21 17:10:02 +0100

    first version

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -10,13 +10,25 @@ function getScript(source, callback) {
 //If a files had no dependencies, simply use...
 //  loadJS('path/to/file.js');
 
-getScript('https://code.jquery.com/jquery-3.2.1.slim.min.js', function(){
+
+
+getScript("https://kit.fontawesome.com/0e9a982e24.js");
+
+getScript('https://code.jquery.com/jquery-3.2.1.min.js', function(){
   // use loadJS('path/to/js/file/with/jQuery/decendencies.js'); here
   getScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', function(){
     // use loadJS('path/to/js/file/with/popper/decendencies.js'); here
     loadJS('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js');
   });
+  loadJS('/assets/OwlCarousel2-2.3.4%202/dist/owl.carousel.min.js',function(){
+	  loadJS('/assets/js/main.js');
+	  
+  });
+  
+  loadJS('/assets/js/aos.js');
 });
+
+
 
 // use the getScript function to load any scripts who have dependencies
 // for example you could use...
---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	
+  AOS.init();	
+  $(".owl-carousel").owlCarousel(
+	  {
+		  "items":1,
+		  "autoplay":true,
+		  "dots":true,
+		  "loop":true
+	  }
+  );
+  
+  
+  // animating anchors
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 200, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  
+  
+});
