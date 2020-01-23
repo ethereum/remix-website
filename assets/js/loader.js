06c8b05ee13dbbaf3660a412cf79016c8018cc86
Filip Mertens <bunsenstraat@gmail.com>
2020-01-23 08:57:02 +0100

    fix js relative

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -12,21 +12,7 @@ function getScript(source, callback) {
 
 
 
-getScript("https://kit.fontawesome.com/0e9a982e24.js");
 
-getScript('https://code.jquery.com/jquery-3.2.1.min.js', function(){
-  // use loadJS('path/to/js/file/with/jQuery/decendencies.js'); here
-  getScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js', function(){
-    // use loadJS('path/to/js/file/with/popper/decendencies.js'); here
-    loadJS('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js');
-  });
-  loadJS('/assets/OwlCarousel2-2.3.4%202/dist/owl.carousel.min.js',function(){
-	  loadJS('/assets/js/main.js');
-	  
-  });
-  
-  loadJS('/assets/js/aos.js');
-});
 
 
 
