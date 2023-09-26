247b00898049cd5337babbc4d0902c1de40e69ae
Liana Husikyan <liana@ethereum.org>
2023-09-26 14:05:10 +0200

    Update EventCard.jsx

--- src/components/ui/EventCard.jsx
+++ src/components/ui/EventCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "swiper/css";
 import "swiper/css/pagination";
-const EventCard = ({name, date, location, description, image, UML }) => {
+const EventCard = ({name, date, location, description, image, URL }) => {
   return (
     <div className="rounded-lg p-3 h-[25.38rem] min-w-[17.28rem]">
       <div className="flex rounded-lg mb-2 h-[17.64rem]">
-        <a href={UML} target="__blank">
+        <a href={URL} target="__blank">
           <img src={image} alt="event" className="w-full rounded-lg mb-2"/>
         </a>
       </div>
-      <a href={UML} className="h5 font-latoBold text-black text-xl mb-1" target="__blank">
+      <a href={URL} className="h5 font-latoBold text-black text-xl mb-1" target="__blank">
         {name}
       </a>
       <h6 className="font-latoBold text-black text-sm mb-1">{`${date} - ${location}`}</h6>
