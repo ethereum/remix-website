93a9e067e61c4406d0e174e1e0ff4b936ea458d4
Liana Husikyan <liana@ethereum.org>
2023-12-13 11:58:45 +0100

    Update video to be a link to ide page

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -22,18 +22,20 @@ const About = () => {
                     and as a learning lab for teaching and experimenting with Ethereum.
                 </p>
                 <img className="m-auto sm:hidden" src={remixEditor} srcSet={`${remixEditor2x} 2x`} alt="remix-ide"/>
-                <div className="hidden sm:block relative m-auto sm:h-[30rem] rounded-[1.25rem] max-w-4xl h-full border-[0.375rem] border-magenta">
-                    <img className="absolute right-14 -top-20 z-10" src={remiIcon} alt="🦔"/>
-                    <ReactPlayer  url={[{src:remixVideo, type: "video/mp4"}]} width='100%'
-                                  className="overflow-hidden absolute rounded-2xl"
-                                  playing={true}
-                                  loop={true}
-                                  muted
-                                  playsinline={true}
-                                  config={{ file: { forceVideo: true } }}
-                                  height='100%'/>
-
-                </div>
+                <a href="https://remix.ethereum.org/" target="__blank">
+                    <div className="hidden sm:block relative m-auto sm:h-[30rem] rounded-[1.25rem] max-w-4xl h-full border-[0.375rem] border-magenta">
+                        <img className="absolute right-14 -top-20 z-10" src={remiIcon} alt="🦔"/>
+                        <ReactPlayer  url={[{src:remixVideo, type: "video/mp4"}]} width='100%'
+                                      className="overflow-hidden absolute rounded-2xl"
+                                      playing={true}
+                                      loop={true}
+                                      muted
+                                      playsinline={true}
+                                      config={{ file: { forceVideo: true } }}
+                                      height='100%'/>
+    
+                    </div>
+                /a>
             </div>
         </Section>
     )
