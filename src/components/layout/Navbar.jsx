0f29694c7a43fe0768b42bb5351c8c949598201e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2024-05-03 16:49:46 -0700

    refactor: navbar spacing

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { ReactComponent as Logo } from '../../assets/images/remix-logo.svg';
+import { ReactComponent as RemixLogo } from '../../assets/images/remix-logo.svg';
 import { ReactComponent as Hamburger } from '../../assets/images/hamburger.svg';
 import { ReactComponent as Close } from '../../assets/images/close.svg';
 import { ReactComponent as DownArrow } from "../../assets/images/down-arrow.svg";
 import { ReactComponent as UpArrow } from "../../assets/images/up-arrow.svg";
 import { ReactComponent as NEArrow } from "../../assets/images/northeast-arrow.svg";
-import { LEARNETH_PLUGIN_TUTORIALS_URL, REMIX_IDE_URL } from "../../constants";
+import { LEARNETH_PLUGIN_TUTORIALS_URL, REMIX_HOME_URL, REMIX_IDE_URL } from "../../constants";
 import ThemeDropdown from "../ui/ThemeDropdown"
 import { useColorMode } from '../../hooks/useColorMode';
 import { getDocsHref } from '../../utils/url';
@@ -51,31 +51,20 @@ const Navbar = () => {
 
     return (
         <div
-            className={`
-                fixed
-                font-helvetica
-                inset-x-0
-                top-0
-                z-50
-                flex
-                items-center
-                ${isMenuOpen ? "h-full" : "shadow-md"}
-                backdrop-blur[3px]
-                blur-backdrop
-            `}>
+            className={` fixed font-helvetica inset-x-0 top-0 z-50 flex items-center ${isMenuOpen ? "h-full" : "shadow-md"} backdrop-blur[3px] blur-backdrop`}>
             <div className={`${isMenuOpen ? "h-full" : ""} sticky w-full top-0 z-10 mx-auto max-w-7xl`}>
                 <div className={`flex ${isMenuOpen ? "flex-col" : ""} w-full mx-auto max-w-7xl items-center justify-between px-8 h-[var(--space-nav-height)]`}>
                     {/* NAVIGATION BAR DESKTOP/MOBILE */}
                     <div className="flex w-full h-[4.25rem] items-center gap-4">
                         {/* DESKTOP MENU ITEMS */}
                         <div className="flex w-full justify-between h-[4.25rem]">
-                            <div className="flex flex-shrink-0 items-center">
-                                <Logo className={`${isMenuOpen ? "hidden" : ""} text-primary h:[30px] w-auto block`} alt="Remix logo" />
-                            </div>
+                            <a href={REMIX_HOME_URL} className="flex flex-shrink-0 items-center">
+                                <RemixLogo className={`${isMenuOpen ? "hidden" : ""} text-primary h:[30px] w-auto block`} alt="Remix logo" />
+                            </a>
                             <div className="hidden sm:flex flex-row gap-7">
                                 <a
                                     className="relative inline-flex hover:cursor-pointer items-center shadow-underline"
-                                    href="/#"
+                                    href={REMIX_HOME_URL}
                                 >
                                     <div
                                         className="text-primary px-2 text-base leading-6 font-normal">
@@ -105,8 +94,8 @@ const Navbar = () => {
                                 </a>
 
                                 <div className=" relative inline-flex items-center" ref={ref}>
-                                    <div className="group relative px-2 text-base leading-6 font-normal text-body /hover:cursor-pointer h-full">
-                                        <button className="inline-flex items-center w-full gap-1.5 h-full group-hover:text-primary hover:shadow-thick-underline" onClick={toggleLearnSection}>
+                                    <div className="group relative text-base leading-6 font-normal text-body /hover:cursor-pointer h-full">
+                                        <button className="inline-flex px-2 items-center w-full gap-1.5 h-full group-hover:text-primary hover:shadow-thick-underline" onClick={toggleLearnSection}>
                                             Learn {isLearnOpen ? <UpArrow /> : <DownArrow />}
                                         </button>
 
@@ -153,7 +142,7 @@ const Navbar = () => {
                         <div className="pl-14 flex flex-col h-full gap-10 sm:hidden">
                             <a
                                 className="relative inline-flex hover:cursor-pointer items-center"
-                                href="/#"
+                                href={REMIX_HOME_URL}
                             >
                                 <div
                                     className={`"text-primary" px-1 pt-1 text-base leading-6 font-normal`}>
